4207a5e50b3dac8f5f9827e2e493f2ce22fcf455
saurabh <mrsaurabhshinde5@gmail.com>
2023-01-28 15:38:12 +0530

    first start programs

---
+++ src/com/day3/EmployeeWages/EmployeeWages.java
@@ -0,0 +1,8 @@
+package com.day3.EmployeeWages;
+
+public class EmployeeWages
+{
+    public static void main(String[] args) {
+        System.out.println("Here is programs start check Employee Wages");
+    }
+}
