be6ab61a5f883dec846c322c2840f84f601dd494
saurabh <mrsaurabhshinde5@gmail.com>
2023-01-28 17:33:36 +0530

    Added partime Employee and wages

--- src/com/day3/EmployeeWages/EmployeeWages.java
+++ src/com/day3/EmployeeWages/EmployeeWages.java
@@ -5,19 +5,24 @@ public class EmployeeWages
     public static void main(String[] args)
     {
         System.out.println("Here is programs start check Employee Wages"); /* write a message for understanding*/
-        int present=0, absent=1, perHoursSallary=20, workingHours=0;
+        int present=0, absent=1, perHoursSallary=20, workingHours=0, parttime=4;
         int check=(int) ((Math.random()*10)%3);
         if(check==present)
         {
             System.out.println("Employee was present");
             workingHours=8;
         }
+        else if(check==parttime)
+        {
+            System.out.println("Employee was Part Time worker");
+            workingHours=4;
+        }
         else
         {
-            System.out.println("Employee was Absent");
+            System.out.println("Employee is Absent");
             workingHours=0;
         }
         int salary=perHoursSallary*workingHours;
-        System.out.println("employee Daily wages is=  "+salary);
+        System.out.println("Employee working hours is = "+workingHours +"."+" And Daily wages is= "+salary);
     }
 }
