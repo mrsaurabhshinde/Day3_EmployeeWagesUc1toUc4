6427757ce96304568b85d285a5734eb0ff569be0
saurabh <mrsaurabhshinde5@gmail.com>
2023-01-28 17:43:44 +0530

    Adding a part time employee and wages

--- src/com/day3/EmployeeWages/EmployeeWages.java
+++ src/com/day3/EmployeeWages/EmployeeWages.java
@@ -5,7 +5,7 @@ public class EmployeeWages
     public static void main(String[] args)
     {
         System.out.println("Here is programs start check Employee Wages"); /* write a message for understanding*/
-        int present=0, absent=1, perHoursSallary=20, workingHours=0, parttime=4;
+        int present=0, absent=1,perHoursSallary=20, workingHours=0, parttime=4;
         int check=(int) ((Math.random()*10)%3);
         if(check==present)
         {
